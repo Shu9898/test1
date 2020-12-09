8044ff036929a97bcfe0ab55a1bf3f7ab1e1738d
linshuping <1018961123@qq.com>
2020-12-09 10:43:40 +0800

    初始化项目

---
+++ src/components/tabbar/MainTabBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="main-tab-bar">
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor="#ffcc00">
+<!--        <img slot="item-icon" src="../../assets/img/tabbar/nav2.png" alt="">-->
+<!--        <img slot="item-icon" src="@/assets/img/tabbar/nav2.png" alt="">-->
+        <img slot="item-icon" src="~assets/img/tabbar/nav2.png" alt="">
+<!--        <img slot="item-icon-active" src="../../assets/img/tabbar/nav1.png" alt="">-->
+<!--        <img slot="item-icon-active" src="@/assets/img/tabbar/nav1.png" alt="">-->
+        <img slot="item-icon-active" src="~assets/img/tabbar/nav1.png" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="#ffcc00">
+        <img slot="item-icon" src="../../assets/img/tabbar/nav2.png" alt="">
+        <img slot="item-icon-active" src="../../assets/img/tabbar/nav1.png" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="#ffcc00">
+        <img slot="item-icon" src="../../assets/img/tabbar/nav2.png" alt="">
+        <img slot="item-icon-active" src="../../assets/img/tabbar/nav1.png" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="#ffcc00">
+        <img slot="item-icon" src="../../assets/img/tabbar/nav2.png" alt="">
+        <img slot="item-icon-active" src="../../assets/img/tabbar/nav1.png" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+  // import TabBar from "./TabBar";
+  // import TabBarItem from "./TabBarItem";
+  import TabBar from "components/tabbar/TabBar";
+  import TabBarItem from "components/tabbar/TabBarItem";
+
+    export default {
+        name: "MainTabBar",
+      components: {
+        TabBar,
+        TabBarItem
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBar"
+  }
+</script>
+
+<style scoped>
+  #tab-bar {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    /*一般tabbar的高度是49px*/
+    height: 49px;
+    background-color: #f4f4f4;
+    box-shadow: 0 -1px 5px rgba(0, 0, 0, .1);
+  }
+</style>
